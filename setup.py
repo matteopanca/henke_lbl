c244054c9bfc6447c08b04d6c4146ed12c1b2824
Matteo <mpancaldi.physics@gmail.com>
2020-11-27 10:35:04 +0100

    Minor change

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name='Henke-LBL',
       version='1.0',
       author='Matteo Pancaldi',
       author_email='mpancaldi.physics@gmail.com',
+      url='https://github.com/matteopanca/henke_lbl',
       packages=['henkelbl'],
      )
